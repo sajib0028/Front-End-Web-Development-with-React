584cd1f9dfd221f29c86a129d860a5e9760358ef
Sajib Biswas <sajibb813@gmail.com>
2021-08-25 15:48:48 +0530

    Presentational and Container Components

--- src/Components/DishedDetails.js
+++ src/Components/DishedDetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "reactstrap";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
 class DishDetail extends Component{
     constructor(props) {
@@ -47,11 +47,7 @@ class DishDetail extends Component{
                     <p>{comment.comment}</p>
                     <p>-- {comment.author},
                     &nbsp;
-                    {new Intl.DateTimeFormat('en-US', {
-                        year: 'numeric',
-                        month: 'long',
-                        day: '2-digit'
-                    }).format(new Date(comment.date))}
+                    {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                     </p>
                 </li>
             )
--- src/Components/MenuComponent.js
+++ src/Components/MenuComponent.js
@@ -32,7 +32,8 @@ class Menu extends Component{
         const menu = this.props.dishes.map((dish) => {
             return (
                 <div key={ dish.id } className="col-12 col-md-5 m-1">    
-                    <Card onClick={ () => this.onDishSelect( dish ) } >
+                    <Card key={dish.id}
+                        onClick={() => this.props.onClick(dish.id)}>
 
                         <CardImg width="100%" src={ dish.image } alt={ dish.name } />
                         <CardImgOverlay>
