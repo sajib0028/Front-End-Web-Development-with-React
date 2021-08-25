f001835c91317495784f4b82625c9fc7b59ed824
Sajib Biswas <sajibb813@gmail.com>
2021-08-25 16:15:54 +0530

    Functional Components

--- src/Components/DishedDetails.js
+++ src/Components/DishedDetails.js
@@ -1,21 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
-class DishDetail extends Component{
-    constructor(props) {
-        super(props);
 
-        console.log(props);
-        
-        // stores iproperties of this component
-        this.state = {
-            selectedDishDetail: this.props.dsdetail
-        };
+ 
 
-
-    }
-
-    renderDish(dish) {
+    function RenderDish({dish}) {
 
         if (dish != null) {
             return (
@@ -37,7 +26,7 @@ class DishDetail extends Component{
         }
     }
 
-    renderComments(comments){
+    function RenderComments({comments}) {
         if (comments == null) {
             return (<div></div>)
         }
@@ -64,26 +53,28 @@ class DishDetail extends Component{
     }
 
 
-    render(){
-        const dish = this.props.dish
+    const  DishDetail = (props) => {
 
-        console.log(dish);
         
+const dish = props.dish
+        
+    
         if (dish == null) {
             return (<div></div>);
         }
 
-        const dishItem = this.renderDish(dish);
-        const dishComment = this.renderComments(dish.comments);
+        
 
         return (
+            
             <div className='row'>
-                {dishItem}
-                {dishComment}
+            <RenderDish dish={ props.dish } />
+            <RenderComments comments={ props.comments } />
             </div>
-        )
+            
+        );
     }
 
-}
+
 
 export default DishDetail;
--- src/Components/MenuComponent.js
+++ src/Components/MenuComponent.js
@@ -1,66 +1,36 @@
-import React, { Component } from "react";
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle  } from "reactstrap";
-import DishDetail from './DishedDetails';
+import React from 'react';
+import { Card, CardImg, CardImgOverlay,
+    CardTitle } from 'reactstrap';
 
-class Menu extends Component{
-
-    constructor(props){
-        super(props);
-
-        // stores iproperties of this component
-        this.state = {
-            selectedDish: null
-        };
-
-        console.log('Menu component constructed');
-        
+    function RenderMenuItem ({dish, onClick}) {
+        return (
+            <Card
+                onClick={() => onClick(dish.id)}>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Card>
+        );
     }
 
-    onDishSelect(dish){
+    const Menu = (props) => {
 
-        this.setState({
-            selectedDish: dish
-        });
-
-    }
-
-
-
-    render(){
-        console.log('renders menu component');
-        
-        const menu = this.props.dishes.map((dish) => {
+        const menu = props.dishes.map((dish) => {
             return (
-                <div key={ dish.id } className="col-12 col-md-5 m-1">    
-                    <Card key={dish.id}
-                        onClick={() => this.props.onClick(dish.id)}>
-
-                        <CardImg width="100%" src={ dish.image } alt={ dish.name } />
-                        <CardImgOverlay>
-                            <CardTitle> { dish.name }</CardTitle>
-                        </CardImgOverlay>
-                    </Card>                
+                <div className="col-12 col-md-5 m-1"  key={dish.id}>
+                    <RenderMenuItem dish={dish} onClick={props.onClick} />
                 </div>
             );
         });
 
-        return(
+        return (
             <div className="container">
                 <div className="row">
-                    { menu }
+                    {menu}
                 </div>
-
-                <DishDetail dish={this.state.selectedDish} />
-                
-
             </div>
         );
     }
-
-    componentDidMount(){
-        console.log('Menu component componentDidMounbt is invoked');
-        
-    }
-}
 
 export default Menu;
